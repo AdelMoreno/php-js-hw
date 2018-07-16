c95cc8fcfc8216408fb7660ecb80325624ad125b
Adel Moreno <adel.argenix@gmail.com>
2018-07-16 16:28:38 -0600

    added more code to functions to make them a little more complex

--- js-hw.js
+++ js-hw.js
@@ -1,20 +1,28 @@
 
 
 function exchange(USD) {
-	return USD * 112.25;
+	if(USD >= 1) {
+		return USD * 112.25;
+	}
+	else if(USD < 1) {
+		return USD * .0089;
+	}
 }
-exchange(2);
-
+exchange(-5);
 
 function combine(word1,word2) {
-	return word1 + word2
+	if(word1.length < 3) {
+		return "Oh no you didn't!";
+	}
+	else return word1 + word2;
 }
 combine("you", "like");
 
 let veggies = ["celery", "cucumber", "beet"];
 
-function woo() {
+function woo(veg) {
+	veggies.push (veg);
 	return veggies.toString();
 }
 
-woo();
+woo("lettuce");
--- php-hw.php
+++ php-hw.php
@@ -1,15 +1,29 @@
 <?php
 function exchange($USD) {
+	if($USD >= 1){
 	echo $USD * 112.25;
+	}
+	else if($USD <=1){
+		echo $USD * .0089;
+	}
 }
 
-echo exchange(2);
+
+echo exchange(.2);
 
 function combine($word1, $word2){
-	echo $word1 ."". $word2;
+	if(strlen($word1)<3){
+		echo " Oh no you didn't";
+	}
+	else {
+		echo " ". $word1 ."". $word2;
+	}
 }
 
-echo combine("you", "missed");
+echo combine("u", "missed");
 
-$veggies = array("celery", "cucumber", "beet");
-echo implode("",$veggies);
+$veggies = array(" celery", " cucumber", " beet");
+array_push($veggies, "lettuce");
+echo implode(" ",$veggies);
+
+?>
